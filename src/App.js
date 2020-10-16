8d671e727bffa1817c35db6fbc80208659f54cde
Arlind Pestisha <arlind.pestisha@hyperisland.se>
2020-10-16 09:44:50 +0200

    Creating the Search component and starting to build it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 // Below all the imports for Components
+import Search from './components/Search';
 
 const App = () => {
   return (
     <div className="App">
       <header>
         <h1>Find your Movie</h1>
-        
       </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
