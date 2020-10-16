ac75076544b0fbe43db5e931bf2e5dce3224b2ef
Arlind Pestisha <arlind.pestisha@hyperisland.se>
2020-10-16 10:34:17 +0200

    Setting up the state and logic for the search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Below all the imports for Components
 import Search from './components/Search';
 
 const App = () => {
+
+  // Using Hooks to set the state
+  const [state, setState] = useState({
+    // s is for search
+    s: "",
+    // results is after we search and we render to the UI
+    results: [],
+    // selected is after we click to the film Poster to give use Info for the film
+    selected: {}
+  });
+
+  // API from omdbapi with a personal API KEY to use
+  const apiurl = "http://www.omdbapi.com/?apikey=13869b4";
+
+  const handleInput = (e) => {
+    let s = e.target.value
+
+    setState(prevState => {
+      return {...prevState, s: s}
+    });
+    console.log(state.s)
+  };
   return (
     <div className="App">
       <header>
         <h1>Find your Movie</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} />
       </main>
     </div>
   );
