effa0af9cfb04a9cb68735ac1b4d4794ec7805b0
Arlind Pestisha <arlind.pestisha@hyperisland.se>
2020-10-18 10:38:09 +0200

    seting up and building the test for the Search component

---
+++ src/Search.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Search from './components/Search'
+
+it('renders correctly', () => {
+    const { queryByPlaceholderText } = render(<Search />)
+
+    expect(queryByPlaceholderText('Search your movie...')).toBeTruthy()
+})
+
+describe("Input value", () => {
+    it("updates on change", () => {
+        const {queryByPlaceholderText} = render(<Search />)
+        const searchInput = queryByPlaceholderText("Search your movie...")
+        
+        fireEvent.change(searchInput, {target: {value: ""}})
+
+        expect(searchInput.value).toBe('')
+    });
+})
