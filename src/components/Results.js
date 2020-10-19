08baf18258a7b58bc2b08027bda31b09af41861b
Arlind Pestisha <arlind.pestisha@hyperisland.se>
2020-10-19 09:32:20 +0200

    passing props for open the movie

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Result from './Result'
 
-const Results = ({ results }) => {
+const Results = ({ results, openMovie }) => {
     return (
         <section className="results"> 
-            {results.map(result => (<Result key={result.imdbID} result={result} />))}
+            {results.map(result => (<Result key={result.imdbID} result={result}  openMovie={openMovie} />))}
         </section>
     )
 }
